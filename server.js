5246f43d1af33a12f0c7d8ac7011ba6f2b6d61e0
Arsh Sohal <arshsohal02@gmail.com>
2023-08-26 19:05:38 +0530

    file-upload-html

--- server.js
+++ server.js
@@ -3,23 +3,24 @@ const multer = require('multer');
 const mime = require('mime-types');
 
 const app = express();
-const port = 3000; // You can change this port if needed
+const port = 3000; 
 
-// Configure multer for file uploads
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
     res.send('Welcome to the file upload API');
 });
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+    next();
+    });
 app.post('/upload', upload.single('file'), (req, res) => {
-    // Check if a file was uploaded
     if (!req.file) {
         return res.status(400).json({ message: 'No file uploaded' });
     }
 
-    // Determine the MIME type of the uploaded file
     const mimeType = mime.lookup(req.file.originalname);
 
     if (!mimeType) {
@@ -30,7 +31,6 @@ app.post('/upload', upload.single('file'), (req, res) => {
         originalname: req.file.originalname,
         mimetype: mimeType,
         size: req.file.size,
-        //print file type
     };
 
     res.status(200).json(fileInfo);
